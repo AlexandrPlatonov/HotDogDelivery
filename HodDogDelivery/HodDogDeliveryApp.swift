63e618d9160eab180c8f94256cbcf027b1497397
Alexandr <alex280295@yandex.ru>
2022-05-18 15:53:41 +0700

    Initial Commit

---
+++ HodDogDelivery/HodDogDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HodDogDeliveryApp.swift
+//  HodDogDelivery
+//
+//  Created by Александр Платонов on 18.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HodDogDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
